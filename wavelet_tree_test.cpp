5a678eb04e5e79475489b6138cf072b6a325248c
Chinmay Agrawal <chinmayagrawal90@gmail.com>
2022-11-30 13:03:26 -0700

    exception handling update

--- wavelet_tree_test.cpp
+++ wavelet_tree_test.cpp
@@ -50,6 +50,7 @@ void string_test() {
 
 int main() {
 	string s = "alabar a la alabarda";
+	char invalid_c = 'z';
 	// string s = "mississippi";
 	wt::wavelet_tree<char> tree(s);
 
@@ -63,8 +64,28 @@ int main() {
 	cout << endl;
 
 	// Access Invalid Input Test
-	cout << "access(" << -3 << ") = " << tree.access(50) << endl;
-	cout << "access(" << 50 << ") = " << tree.access(50) << endl;
+	try 
+	{
+		cout << "access(" << -3 << ") = " << tree.access(50) << endl;
+	}
+	catch(const std::out_of_range& e)
+	{
+		std::cout << e.what() << '\n';
+		// return 0;
+	}
+	cout << endl;
+
+	try 
+	{
+		cout << "access(" << 50 << ") = " << tree.access(50) << endl;
+	}
+	catch(const std::out_of_range& e)
+	{
+		std::cout << e.what() << '\n';
+		// return 0;
+	}
+	cout << endl;
+	
 
 	// Rank Test
 	for (char c : set<char>(s.begin(), s.end()))
@@ -72,11 +93,30 @@ int main() {
 	cout << endl;
 
 	//Rank Invalid Input Test
-	// cout << tree.rank('z', 3) << endl;
+	try 
+	{
+		cout << "rank(" << invalid_c << ", 3" << ") = " << tree.rank(invalid_c, 3) << endl;
+	}
+	catch(const std::out_of_range& e)
+	{
+		cout << e.what() << endl;
+	}
+	cout << endl;
 
 	// Select Test
 	for (char c : set<char>(s.begin(), s.end()))
 		cout << "select(" << c << ", " << 1 << ") = " << tree.select(c, 1) << endl;
+	cout << endl;
+
+	// Select Invalid Input Test
+	try 
+	{
+		cout << "select(" << invalid_c << ", 3" << ") = " << tree.rank(invalid_c, 3) << endl;
+	}
+	catch(const std::out_of_range& e)
+	{
+		cout << e.what() << endl;
+	}
 	cout << endl;
 
 	
